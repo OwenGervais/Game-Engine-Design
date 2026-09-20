55a968aed621f44ebe238c98c752fd301801b68f
OwenGervais <owen.gervais@ontariotechu.net>
2026-09-19 22:59:08 -0400

    Added 3d model to mess around with.

---
+++ Assets/Spin.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    private Transform transform;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
